4da7995ff769ecd7c5c942456538a92638595e87
UrsalVince <ursalvinceneil@gmail.com>
2024-07-06 22:59:21 +0800

    NEW PROJECT NEEDS FIXING

---
+++ Entities/LoginTb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JJK.Entities;
+
+public partial class LoginTb
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public bool RememberMe { get; set; }
+}
